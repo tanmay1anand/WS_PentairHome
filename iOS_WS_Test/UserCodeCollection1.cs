783320e4030ba518251c040bda442ae27e6c8299
tanmay1anand <32032654+tanmay1anand@users.noreply.github.com>
2020-10-07 00:36:53 +0530

    first commit

---
+++ iOS_WS_Test/UserCodeCollection1.cs
@@ -0,0 +1,43 @@
+﻿/*
+ * Created by Ranorex
+ * User: Tanmay.anand
+ * Date: 7/21/2020
+ * Time: 3:56 PM
+ * 
+ * To change this template use Tools > Options > Coding > Edit standard headers.
+ */
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Drawing;
+using System.Threading;
+using WinForms = System.Windows.Forms;
+
+using Ranorex;
+using Ranorex.Core;
+using Ranorex.Core.Testing;
+
+namespace iOS_WS_Test
+{
+    /// <summary>
+    /// Creates a Ranorex user code collection. A collection is used to publish user code methods to the user code library.
+    /// </summary>
+    [UserCodeCollection]
+    public class UserCodeCollection1
+    {
+        // You can use the "Insert New User Code Method" functionality from the context menu,
+        // to add a new method with the attribute [UserCodeMethod].
+        /// <summary>
+        /// This is a placeholder text. Please describe the purpose of the
+        /// user code method here. The method is published to the user code library
+        /// within a user code collection.
+        /// </summary>
+        [UserCodeMethod]
+        public static void SendValues()
+        {
+        	
+        }
+        
+    }
+}
